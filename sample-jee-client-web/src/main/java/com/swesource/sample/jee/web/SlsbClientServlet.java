0beb737d0e81eecd0bfcdf37ff70b8f730696b4d
Arnold Johansson <arnie@swesource.com>
2012-05-21 13:28:43 +0200

    Correct use of affinity in client for utilising own ClusterNodeSelector

--- sample-jee-client-web/src/main/java/com/swesource/sample/jee/web/SlsbClientServlet.java
+++ sample-jee-client-web/src/main/java/com/swesource/sample/jee/web/SlsbClientServlet.java
@@ -4,6 +4,7 @@ import com.swesource.sample.jee.RelaySlsbRemote;
 import com.swesource.sample.jee.ServerSlsbRemote;
 
 import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,12 +19,34 @@ import java.util.Hashtable;
  */
 @WebServlet(value = "/SlsbClientServlet")
 public class SlsbClientServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        invokeOnBean();
+
+    private ServerSlsbRemote serverSlsbProxy;
+    private RelaySlsbRemote relaySlsbProxy;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            final Hashtable properties = new Hashtable();
+            properties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+            final Context context = new InitialContext(properties);
+
+            // Lookup the beans using the ejb: namespace syntax which is explained here
+            // https://docs.jboss.org/author/display/AS71/EJB+invocations+from+a+remote+client+using+JNDI
+
+            String serverSlsbPath = "ejb:sample-jee-server/sample-jee-server-ejb-1.0-SNAPSHOT//ServerSlsbBean!com.swesource.sample.jee.ServerSlsbRemote";
+            serverSlsbProxy = (ServerSlsbRemote) context.lookup(serverSlsbPath);
+
+            String relaySlsbPath = "ejb:sample-jee-relay/sample-jee-relay-ejb-1.0-SNAPSHOT//RelaySlsbBean!com.swesource.sample.jee.RelaySlsbRemote";
+            relaySlsbProxy = (RelaySlsbRemote) context.lookup(relaySlsbPath);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String greeting = invokeOnBean();
+        String greeting = invokeOnServerBean();
         PrintWriter pw = response.getWriter();
         pw.write("SlsbClientServlet -> ServerSlsbBean.sayHello (in remote EAR) -> SlsbClientServlet -> [" + greeting + "]\n");
         //String callResponse = invokeOnRelayBean();
@@ -32,46 +55,15 @@ public class SlsbClientServlet extends HttpServlet {
         pw.close();
     }
 
-    private String invokeOnBean() {
-        try {
-            final Hashtable props = new Hashtable();
-            // setup the ejb: namespace URL factory
-            props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-            // create the InitialContext
-            final Context context = new javax.naming.InitialContext(props);
-
-            // Lookup the SlsbOne bean using the ejb: namespace syntax which is explained here
-            // https://docs.jboss.org/author/display/AS71/EJB+invocations+from+a+remote+client+using+JNDI
-            
-            // For remote ejbs (other container)
-            String path = "ejb:sample-jee-server/sample-jee-server-ejb-1.0-SNAPSHOT//ServerSlsbBean!com.swesource.sample.jee.ServerSlsbRemote";
-            final ServerSlsbRemote bean = (ServerSlsbRemote) context.lookup(path);
-            
-            // invoke on the bean
-            final String greeting = bean.sayHello();
-            System.out.println("SlsbClientServlet: Received greeting: " + greeting);
-            return greeting;
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    private String invokeOnServerBean() {
+        String greeting = serverSlsbProxy.sayHello();
+        System.out.println("SlsbClientServlet: Received greeting: " + greeting);
+        return greeting;
     }
 
     private String invokeOnRelayBean() {
-        try {
-            final Hashtable props = new Hashtable();
-            props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-            final Context context = new javax.naming.InitialContext(props);
-            String path = "ejb:sample-jee-relay/sample-jee-relay-ejb-1.0-SNAPSHOT//RelaySlsbBean!com.swesource.sample.jee.RelaySlsbRemote";
-            final RelaySlsbRemote bean = (RelaySlsbRemote) context.lookup(path);
-
-            // invoke on the bean
-            final String callResponse = bean.callEjbInSameAppSrvButOtherEar();
-            System.out.println("SlsbClientServlet: Received call response: " + callResponse);
-            return callResponse;
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        final String callResponse = relaySlsbProxy.callEjbInSameAppSrvButOtherEar();
+        System.out.println("SlsbClientServlet: Received call response: " + callResponse);
+        return callResponse;
     }
 }
